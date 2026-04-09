4462e18e40b1819fb6c1946cafc135c4c3d7a67e
bepoisso <github@bepoisso.com>
2026-04-09 16:51:51 +0200

    ADD todo list

---
+++ Module-04/ex03/main.cpp
@@ -0,0 +1,7 @@
+// todo: Le main de démo demandé est vide : main.cpp.
+// todo: Le comportement médiateur complet du Headmaster n’est pas encore implémenté (orchestration prof/étudiant/cours/salle, pas seulement signer/exécuter un Form) : Headmaster.cpp:13-68.
+// todo: Le flux “prof sans cours ⇒ création d’un cours” n’est pas terminé.
+// todo: Le flux “pas de salle libre ⇒ création de classroom” n’est pas terminé.
+// todo: Le flux “étudiant non inscrit ⇒ tentative d’inscription” n’est pas terminé.
+// todo: Le flux “graduation après nombre de classes requis, sur recommandation du prof” n’est pas terminé.
+
