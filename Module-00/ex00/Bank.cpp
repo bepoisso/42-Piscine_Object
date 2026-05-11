174c3bf2352e818bf1699c538808be02f4a1e7f6
bepoisso <github@bepoisso.com>
2026-05-11 15:58:51 +0200

    fix: nullptr error

--- Module-00/ex00/Bank.cpp
+++ Module-00/ex00/Bank.cpp
@@ -93,7 +93,7 @@ void Bank::withdraw(int id, int minudValue) {
 
 const Account* Bank::operator[](int id) const {
 	Account* client = clientAccounts[id];
-	return client ? client : nullptr;
+	return client ? client : NULL;
 }
 
 
