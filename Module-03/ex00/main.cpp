0d1460215739a9dd10fc3c1da291458fcc7cd0bc
bepoisso <github@bepoisso.com>
2026-06-17 17:16:46 +0200

    update: improve output readability

--- Module-03/ex00/main.cpp
+++ Module-03/ex00/main.cpp
@@ -4,27 +4,34 @@
 int main() {
 	std::cout << "=== TEST SUBSYSTEMS ===" << std::endl;
 
+
+	std::cout << "--- Engine ---" << std::endl;
 	Engine engine;
 	engine.start();
 	engine.stop();
 
+	std::cout << std::endl << "--- Gearbox ---" << std::endl;
 	Gearbox gearbox;
 	gearbox.shift_gears_up();
 	gearbox.shift_gears_down();
 	gearbox.reverse();
 
+	std::cout << std::endl << "--- SteeringSystem ---" << std::endl;
 	SteeringSystem steering;
-	steering.turn_wheel(25.0f);
+	steering.turn_wheel(25.5f);
 	steering.straighten_wheels();
 
+	std::cout << std::endl << "--- BrakeSystem ---" << std::endl;
 	BrakeSystem brakes;
 	brakes.apply_force_on_brakes(120.0f);
 	brakes.apply_emergency_brakes();
 
+	std::cout << std::endl << "--- SpeedController ---" << std::endl;
 	SpeedController speed;
 	speed.accelerate(60.0f);
 
-	std::cout << "\n=== TEST CAR (ORCHESTRATION) ===" << std::endl;
+
+	std::cout << std::endl << "\n=== TEST CAR (ORCHESTRATION) ===" << std::endl;
 
 	Car car;
 	car.start();
